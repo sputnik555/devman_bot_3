016c6fffe7834f3f4f96730bc7c6a02593dbb2ad
Alexey Beletsky <sputnik2009@gmail.com>
2022-10-01 15:01:53 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from environs import Env
+from google.cloud import dialogflow
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -30,14 +31,17 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(google_project_name, update.message.chat_id)
+    text_input = dialogflow.TextInput(text=update.message.text, language_code='ru')
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    update.message.reply_text(response.query_result.fulfillment_text)
 
 
 def main() -> None:
-    env = Env()
-    env.read_env()
-    tg_token = env.str('TELEGRAM_TOKEN')
-
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
     updater = Updater(tg_token)
@@ -62,4 +66,8 @@ def main() -> None:
 
 
 if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    tg_token = env.str('TELEGRAM_TOKEN')
+    google_project_name = env.str('GOOGLE_PROJECT_NAME')
     main()

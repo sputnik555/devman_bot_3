e26c62054e9a28ae81acfc447ec55fe48f0f3c5c
Alexey Beletsky <sputnik2009@gmail.com>
2022-10-03 01:43:37 +0300

    add DialogFlow to VK bot.

--- vk_bot.py
+++ vk_bot.py
@@ -2,14 +2,22 @@ import random
 
 import vk_api
 from environs import Env
+from google.cloud import dialogflow
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-def echo(event, vk_api):
+def reply(event, vk_api, google_project_name):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(google_project_name, event.user_id)
+    text_input = dialogflow.TextInput(text=event.text, language_code='ru')
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+        message=response.query_result.fulfillment_text,
+        random_id=random.randint(1, 1000)
     )
 
 
@@ -17,10 +25,11 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
     vk_token = env.str('VK_TOKEN')
+    google_project_name = env.str('GOOGLE_PROJECT_NAME')
     vk_session = vk_api.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            reply(event, vk_api, google_project_name)
